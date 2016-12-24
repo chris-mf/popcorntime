f72d2de2b5cecb785036458dc2c9dd781f024268
Your Name <chris-ellis@mentallyfriendly.com>
2016-12-24 22:39:45 +1100

    Hover links and animation

--- src/js/app.js
+++ src/js/app.js
@@ -13,6 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
 }, false);
 
 
+//Smooth Srolling
 
 $(function() {
   $('a[href*=#]:not([href=#])').click(function() {
@@ -28,3 +29,19 @@ $(function() {
     }
   });
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var elements = document.getElementsByTagName("INPUT");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].oninvalid = function(e) {
+            e.target.setCustomValidity("");
+            if (!e.target.validity.valid) {
+                e.target.setCustomValidity("This field cannot be left blank");
+            }
+        };
+        elements[i].oninput = function(e) {
+            e.target.setCustomValidity("");
+        };
+    }
+})
