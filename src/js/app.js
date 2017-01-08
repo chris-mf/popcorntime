b9d7d2e9f098e4582ec6f731cf4a6900a2101fb7
Your Name <chris-ellis@mentallyfriendly.com>
2017-01-09 09:26:09 +1100

    drop down menu js - media Q cleanup

--- src/js/app.js
+++ src/js/app.js
@@ -13,9 +13,19 @@ document.addEventListener('DOMContentLoaded', function() {
 }, false);
 
 
-//Smooth Srolling
+// mobile menu
 
 $(function() {
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    $("nav").hide()
+  }
+
+  $('#nav-toggle').click(function() {
+    $("nav").fadeToggle()
+  })
+//Smooth Srolling
+
+
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
@@ -32,6 +42,9 @@ $(function() {
 
 
 document.addEventListener("DOMContentLoaded", function() {
+
+
+
     var elements = document.getElementsByTagName("INPUT");
     for (var i = 0; i < elements.length; i++) {
         elements[i].oninvalid = function(e) {
